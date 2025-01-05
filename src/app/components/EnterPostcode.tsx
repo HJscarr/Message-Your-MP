fdc781baf4390485af8af1a0b9039d9616f33cc5
HJscarr <jamesscarr6@gmail.com>
2025-01-05 12:15:53 +0000

    bugfix: fix gradient button

--- src/app/components/EnterPostcode.tsx
+++ src/app/components/EnterPostcode.tsx
@@ -5,6 +5,7 @@ import { useMPDetails } from '@/hooks/useFindEmail';
 import EmailContents from './EmailContents';
 import { ClipboardDocumentIcon } from '@heroicons/react/24/outline';
 import Notification from './Notification';
+import GradientButton from './GradientButton';
 
 export default function EnterPostcode() {
   const { loading, error, mpDetails, getMPDetails } = useMPDetails();
@@ -46,13 +47,13 @@ export default function EnterPostcode() {
               className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
             />
           </div>
-          <button
+          <GradientButton
             type="submit"
             disabled={loading}
-            className="mt-4 w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50"
+            className="mt-4 w-full justify-center"
           >
             {loading ? 'Finding MP...' : 'Find my MP'}
-          </button>
+          </GradientButton>
         </form>
 
         {error && (
