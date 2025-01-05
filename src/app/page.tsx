10ee16d65bc059ead7ccf6f2fa7898ff42da751a
HJscarr <jamesscarr6@gmail.com>
2025-01-05 12:18:03 +0000

    bugfix: fix gradient button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { EnvelopeIcon } from '@heroicons/react/24/outline'
 import EnterPostcode from './components/EnterPostcode'
 import InspiredBy from './components/InspiredBy'
 import { EmailProvider, useEmail } from './components/EmailContents'
-import GradientButton from './components/GradientButton'
 
 function MailtoButton() {
   const { emailBody } = useEmail();
